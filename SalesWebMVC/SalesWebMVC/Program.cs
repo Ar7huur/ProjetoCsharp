4589ee082de3b2f50afb1003d0c5269c1febafc0
Arthur <arthursmf@live.com>
2023-05-29 20:10:31 -0300

    Stage2-NovaIntegracao
    Arrumar o SQL Server

--- SalesWebMVC/SalesWebMVC/Program.cs
+++ SalesWebMVC/SalesWebMVC/Program.cs
@@ -6,21 +6,20 @@ using System.Configuration;
 //Program & Startup
 
 var builder = WebApplication.CreateBuilder(args);
-//builder.Services.AddDbContext<SalesWebMVCContext>(options =>
-//options.UseMySql(builder.Configuration.GetConnectionString("SalesWebMVCContext"), builder => builder.MigrationsAssembly("SalesWebMVC")));
-
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<DbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 //string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 //builder.Services.AddDbContext<SalesWebMVCContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 
 //conexão para o MySQL - passando local host, database name, userid, password & versão do mysql
-builder.Services.AddDbContext<SalesWebMVCContext>(options => 
-options.UseMySql("server=localhost;database = saleswebmvcappdb; uid=root;pwd=1234567", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.33-mysql")));
+//builder.Services.AddDbContext<SalesWebMVCContext>(options => 
+//options.UseMySql("server=localhost;database = saleswebmvcappdb; uid=root;pwd=1234567", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.33-mysql")));
 
 
 var app = builder.Build();
-
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
