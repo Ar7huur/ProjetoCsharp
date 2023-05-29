00816494ac842b898164f642781b95eef0143c1d
Arthur <arthursmf@live.com>
2023-05-29 15:17:39 -0300

    Stage1-Parte6
    Conexão com o BD MySQL - OK

--- SalesWebMVC/SalesWebMVC/Program.cs
+++ SalesWebMVC/SalesWebMVC/Program.cs
@@ -3,29 +3,27 @@ using Microsoft.Extensions.DependencyInjection;
 using SalesWebMVC.Data;
 using System.Configuration;
 
+//Program & Startup
+
 var builder = WebApplication.CreateBuilder(args);
 //builder.Services.AddDbContext<SalesWebMVCContext>(options =>
-    //options.UseMySql(builder.Configuration.GetConnectionString("SalesWebMVCContext"), builder => builder.MigrationsAssembly("SalesWebMVC")));
+//options.UseMySql(builder.Configuration.GetConnectionString("SalesWebMVCContext"), builder => builder.MigrationsAssembly("SalesWebMVC")));
 
-//string conexão para o MySQL
+
 //string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 //builder.Services.AddDbContext<SalesWebMVCContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 
-//sdadwd
+//conexão para o MySQL - passando local host, database name, userid, password & versão do mysql
 builder.Services.AddDbContext<SalesWebMVCContext>(options => 
-options.UseMySql("server=localhost;inital catalog=saleswebmvcappdb; uid=root;pwd=1234567",Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.33-mysql")));
+options.UseMySql("server=localhost;database = saleswebmvcappdb; uid=root;pwd=1234567", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.33-mysql")));
 
 
 var app = builder.Build();
 
 
-
-
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
-
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
