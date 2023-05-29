4209df10a0d41e8144907558946eaa731e84fd0d
Arthur <arthursmf@live.com>
2023-05-29 14:55:59 -0300

    Stage1-Parte5
    Connect

--- SalesWebMVC/SalesWebMVC/Program.cs
+++ SalesWebMVC/SalesWebMVC/Program.cs
@@ -7,8 +7,14 @@ var builder = WebApplication.CreateBuilder(args);
 //builder.Services.AddDbContext<SalesWebMVCContext>(options =>
     //options.UseMySql(builder.Configuration.GetConnectionString("SalesWebMVCContext"), builder => builder.MigrationsAssembly("SalesWebMVC")));
 
-string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<SalesWebMVCContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+//string conexão para o MySQL
+//string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+//builder.Services.AddDbContext<SalesWebMVCContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+
+//sdadwd
+builder.Services.AddDbContext<SalesWebMVCContext>(options => 
+options.UseMySql("server=localhost;inital catalog=saleswebmvcappdb; uid=root;pwd=1234567",Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.33-mysql")));
 
 
 var app = builder.Build();
