970a46c9956c6f922ddb15c6495f7ec3ef636976
Arthur <arthursmf@live.com>
2023-05-30 02:25:58 -0300

    Bootstrap colocado.
    Bootstrap flatly colocado no projeto para Direct Dt.

--- SalesWebMVC/SalesWebMVC/Program.cs
+++ SalesWebMVC/SalesWebMVC/Program.cs
@@ -3,13 +3,9 @@ using Microsoft.Extensions.DependencyInjection;
 using SalesWebMVC.Data;
 using System.Configuration;
 
-//Program & Startup
-
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<SalesWebMVCContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+//problema do Erro HTTPS
+// Add services to the container.
+//builder.Services.AddControllersWithViews();
 //string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 //builder.Services.AddDbContext<SalesWebMVCContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
@@ -19,10 +15,15 @@ options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectio
 //options.UseMySql("server=localhost;database = saleswebmvcappdb; uid=root;pwd=1234567", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.33-mysql")));
 
 
-var app = builder.Build();
 
-// Add services to the container.
-//builder.Services.AddControllersWithViews();
+//Program & Startup
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<SalesWebMVCContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
